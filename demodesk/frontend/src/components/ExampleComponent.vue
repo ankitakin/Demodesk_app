927a5d5632149323f770fb999858255f52243def
Ankita <ankita.kinnerkar@tum.de>
2020-08-04 03:29:16 +0200

    Initial commit

---
+++ demodesk/frontend/src/components/ExampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1>Demodesk</h1>
+    <p align="center">
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+  h1 {
+    text-align: center;
+  }
+</style>
